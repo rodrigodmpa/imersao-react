93ff61381b81d215652067e31dfd3d1124ebb99d
Rodrigo Demetrio Palma <rodrigo.demetrio.palma@gmail.com>
2020-07-27 19:50:39 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+
+
+
+
 
 function App() {
   return (
     <div>
     <Menu />
+
+    <BannerMain
+     videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
+     url={dadosIniciais.categorias[0].videos[0].url}
+     videoDescription={"O que é FrontEnd? Trabalhando na área de frontend."}
+    />
+    <Carousel 
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[0]}/>
+    <Carousel 
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[1]}/>
+    <Carousel 
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[2]}/>
+    <Carousel 
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[3]}/>
+    <Carousel 
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[4]}/>
+    <Carousel 
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[5]}/>
     </div>
   );
 }
